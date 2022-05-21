3db391b5ca07eeb139a4f572433b230646bd33ae
DANIELGEDAMO <danielgadamo@gmail.com>
2022-05-21 23:22:57 +0300

    n

--- main.js
+++ main.js
@@ -3,6 +3,13 @@
 //     return str.toUpperCase()
 //  }
 // 2
-function numbers (str) {
-    return Araay[i]
-}
+// var NewAraay= []
+// function numbers (str) {
+//     for (var i =0;i<NewAraay.length;i++){
+//         if (i*0==0){
+//         NewAraay+=i
+//        }
+//     }
+//   return NewAraay
+// }
+// 
