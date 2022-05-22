748a56d037adb5c784ade5723f00fb4e256f80de
DANIELGEDAMO <danielgadamo@gmail.com>
2022-05-23 00:04:25 +0300

    ח

--- main.js
+++ main.js
@@ -13,3 +13,43 @@
 //   return NewAraay
 // }
 // 
+
+
+// function getString (str){
+// for(var i=0; i<str.length;i++){
+//     if( str.length>6){
+//         return long 
+//     }
+//     else{
+//         return short
+//     }
+// }
+// }getString('gkdemt')
+
+
+// function getloction (str){
+// for (var i = 0; i < str.length; i++ ){
+//     if(str[i]=='a'){
+//         return i;
+//     }    
+// }
+//     return 'NOT Foud';
+  
+// }getloction('ddass');
+// function returnTrueOrFalse(str){
+//  for(var i = 0; i<str.length; i++){
+//      if (str[i]=='r'){
+//          return true
+//      }
+    
+//     }
+//      else{
+//         return false
+//        }   
+    
+// }
+// returnTrueOrFalse('daniel gadamo')
+
+function srtingAnd (str,){
+
+}
